c4be49502529e35584c9e0a56390a0e9a3990e4e
geun hoo <boysheep@naver.com>
2025-12-30 17:12:27 +0900

    report 갔다가 reader로 왔을때 로딩x

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import {HashRouter, Routes, Route} from 'react-router-dom';
-import {BookProvider} from './contexts/BookContext';
-import {ReaderPage} from './pages/ReaderPage';
-import {ReportPage} from './pages/ReportPage';
+import React from "react";
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BookProvider } from "./contexts/BookContext";
+import { ReaderPage } from "./pages/ReaderPage";
+import { ReportPage } from "./pages/ReportPage";
+
+const PersistentReader: React.FC = () => {
+  const location = useLocation();
+  const hideReader = location.pathname === "/report";
+
+  return (
+    <div style={{ display: hideReader ? "none" : "block" }} aria-hidden={hideReader}>
+      <ReaderPage />
+    </div>
+  );
+};
+
+const AppRoutes: React.FC = () => {
+  return (
+    <>
+      {/* Keep the Reader mounted so PDF state persists when navigating away */}
+      <PersistentReader />
+      <Routes>
+        <Route path="/report" element={<ReportPage />} />
+        <Route path="*" element={null} />
+      </Routes>
+    </>
+  );
+};
 
 const App: React.FC = () => {
   return (
     <HashRouter>
       <BookProvider>
-        <Routes>
-          <Route path="/" element={<ReaderPage />} />
-          <Route path="/report" element={<ReportPage />} />
-        </Routes>
+        <AppRoutes />
       </BookProvider>
     </HashRouter>
   );
